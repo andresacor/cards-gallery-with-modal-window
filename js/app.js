ab4575e7622a4051b52b1e8dfa8ba9933beda490
Andrés Aristizabal <andresaristizabal777@gmail.com>
2019-09-07 21:39:34 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,96 @@
+
+
+let modal = document.querySelector('.modal')
+let modalImage = document.querySelector('.modal-image ')
+let image = document.querySelector('.image')
+let btnVer = document.querySelectorAll('.leer-mas')
+let text = document.querySelector('.text')
+
+modal.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+btnVer[0].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-1.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[1].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-2.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[2].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-3.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[3].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-4.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[4].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-5.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[5].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-6.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[6].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-7.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+
+btnVer[7].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-8.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+
+btnVer[8].addEventListener('click', function(e){
+    e.preventDefault()  
+    modal.style.display = 'block'
+    modalImage.style.display = 'block'
+    image.setAttribute('src', '../img/sport-9.jpg')
+    text.innerHTML='Lorem ipsum dolor amet ennui offal kinfolk, beard portland whatever celiac mumblecore health goth echo park kale chips.'
+    text.classList.add('text')
+})
+    
+
+
